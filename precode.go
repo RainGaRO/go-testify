7320948ee148895f3ea58e9c3a6bbaa88e211aa1
RainGaRO <helloween06@mail.ru>
2024-05-30 23:33:55 +0300

    second commit

--- precode.go
+++ precode.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
 )
 
 var cafeList = map[string][]string{
@@ -44,25 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=2^city=moscow", nil) // здесь нужно создать запрос к сервису
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	// здесь нужно добавить необходимые проверки
-	if status := responseRecorder.Code; status != http.StatusOK {
-		t.Fatalf("%d, %d", http.StatusOK, status)
-	}
-
-	body := responseRecorder.Body.String()
-	list := strings.Split(body, ",")
-
-	if len(list) != totalCount {
-		t.Errorf("%d, %d", totalCount, len(list))
-	}
 }
